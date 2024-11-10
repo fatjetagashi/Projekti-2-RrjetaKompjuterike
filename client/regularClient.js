4c5a8d655b3cf23fc2617e55bf9dfeb4fb024d78
Fatjeta <fatjeta.gashi3@student.uni-pr.edu>
2024-11-10 13:14:34 +0100

    adminClient.js dhe regularClient.js per krahasim se cili ka kohe pergjigje me te shpejte nga serveri

---
+++ client/regularClient.js
@@ -0,0 +1,23 @@
+const dgram = require('dgram');
+const client = dgram.createSocket('udp4');
+
+const message = Buffer.from('RegularUser; user123; write testRegular.txt Mesazh nga klienti i zakonshëm');
+const serverPort = 41234;
+const serverIp = 'localhost';
+
+const startTime = Date.now(); 
+
+client.send(message, 0, message.length, serverPort, serverIp, (err) => {
+    if (err) {
+        console.error('Gabim gjate dergimit te mesazhit:', err);
+    } else {
+        console.log(`Mesazhi i klientit te zakonshem u dergua ne kohen: ${new Date(startTime).toISOString()}`);
+    }
+
+ 
+    client.on('message', (msg) => {
+        const endTime = Date.now(); //Koha e marrjes se pergjigjes
+        console.log(`Pergjigjja per klientin e zakonshem u mor pas ${endTime - startTime} ms ne kohen: ${new Date(endTime).toISOString()}`);
+        client.close();
+    });
+});
